3c865213d2c38b64aadc23b847a629156f53c338
ElsaChimba <elsachimba28@gmail.com>
2026-07-07 20:52:33 +0100

    Corrigir rota do roteador industrial

--- src/lib/precos.ts
+++ src/lib/precos.ts
@@ -1,116 +1,220 @@
 export const precosProdutos: Record<string, number> = {
-  "Apple iPhone 16 Pro": 950000,
-  "IMPRESSORA HP (DESKJET)": 68000,
-  "TP-LINK 300mbps wirellss N router": 24910,
-  "Monitores wintech 27": 123500,
-  "Auriculares com fio (AEG)": 15000,
-  "TP-LINK PoE Switch": 87210,
-  "TP-LINK Omada Controller": 200000,
-  "TP-LINK Omada Hardware Controller OC200": 204000,
-  "Teclado+ Rato GENIUS": 17040,
-  "DVD-R HP": 19000,
-  "Pendrive(Maxell)": 6000,
-  "Cartão de Memoria 32GB": 4000,
-  "Resma de Papel": 4800,
-  "HDMI Conversor VGA": 9000,
-  "Orga.cabos (EWENT)": 18000,
-  "Conector RJ45": 25000,
-  "Base de Mause (EWENT)": 5000,
-  "Camera Dayhua": 29000,
-  "Camera smarth": 18500,
-  "TP-LINK AC1200": 200000,
+
+  "Apple iPhone 16 Pro": 2100000,
+ 
+  "Portátil HP EliteBook Recondicionado Grande (A)": 495000,
+
   "Memoria RAM DDR4 8Gb 3200A Para Portàtil": 138000,
-  "Telefone Analogico (IPPRO )": 9000,
-  "Auricular sem fio Redmi": 18000,
-  "Alicate Grimpar": 18000,
-  "Cabo de carregar 3 em 1": 4800,
-  "Gaveta de dinheiro": 85000,
-  "UPS Pulstar Line Interactive UPS": 78000,
-
-  // restantes permanecem exatamente iguais
-
-  "TECLADO NGS": 13000,
-  "LB-LINK 6em1 High Gain Isp Smart": 40000,
-  "Cabo HDTV 2k*4k": 6000,
-  "CABO DE REDE CAT6 2 METRO UTP CINZA": 3750,
-  "XPRIT (impressora termica )": 80000,
-  "Cabo Cat6 Networking": 1890,
-  "Access Point TP-LINK EAP225 AC1200 Mbps": 110710,
-  "Cabo Manhattan/cabo de impressora": 3000,
-  "Rolos de TPA": 500,
-  "PILHA MAXELL": 600,
-  "Adaptador USB Micro": 3000,
-  "pilha Maxell Alcaline": 1500,
-  "Carregador Android": 1500,
-  "Cabo HDMI 3 M": 45000,
-  "Cabo HDMI 1.8M": 6000,
-  "Cabo de rede União RJ45": 4000,
-  "Cabo Mini Display Port p/HDMI 4k60Hz Manhattan": 19000,
   "Disco Duro": 130000,
-  "Disco duro recondicionado": 30000,
   "Disco duro Novo": 60000,
-  "Memoria de Rede": 20000,
-  "Capa de memoria": 10000,
-  "Cabo Disco Duro Sata": 5000,
-  "Cabo de Força": 5000,
-  "Cabo Sata": 6500,
-  "Adaptador de Exibiçâo MINI": 8000,
-  "Cabo VGA 1.5M": 5000,
-  "Base de carregador tipo c": 4500,
-  "Impressora termica BIXOLON": 45000,
-  "Extensão USB 2.O 30M": 19000,
-  "Verbatim (DVR)": 19000,
-  "Extensâo UPS(EWENT)": 15000,
-  "Hikvision DVR DS-7216HGHI-M1/T 16 Canais Turbo HD 1080p Lite": 87552,
+  "Disco duro recondicionado": 30000,
+
+  "Pendrive(Maxell)": 6000,
+  "Cartão de Memoria 32GB": 3500,
+"Cartão de memória Micro SD 32 GB kingston canvas select Plus+ Adaptador /class 10": 3500,
+"Relogio Biometrico ZKTeco K20 1xLAN, 1xUSB, 1500 Registro c/ Bateria": 280000,
+"Adaptador de vido VGA para HDMI":25880,
+"Rato sem fio HP preto":10000,
+"Adaptador Wi-Fi Alfa":15000,
+"Roteador Industrial Teltonika RUTM": 120000,
+  "CD-R": 25000,
+  "P.R CD-R 700MB": 25000,
+"Gaveta POS Bematech": 57000,
+"Testador Finder de cabo UTP": 68000,
+"Apple iPhone 17 Pro Max 256 GB":2100000,
+"Monitores wintech 27": 123500,
+"Conector de Rede Intellinet, RJ45 cat 6 100un": 25000,
+"Impressora HP Deskjet AIO 2320 7.5 PPMC/Fio": 68000,
+"Universal Notebook Power Adaptador 8 conectores" : 40000,
+"Repetidor de Sinal Wi-Fi com 2 Antenas Externas":20000,
+"Router Wi-Fi TP-Link 300 Mbps com 2 Antenas, 1 Porta WAN e 4 Portas LAN": 24910,
+"Mini Teclado Controlo de Acesso TV": 13000,
+
+  "IMPRESSORA HP (DESKJET)": 68000,
+  "Impressora HP laserjet MFp color M3303FDw 25/25 PP": 475000,
+  "Proposta de aluguer de impressora": 790000,
+
   "HP LASERJET(305A)TONER": 120000,
   "JET INTELLIGENCE (201A)": 107000,
   "LASERJET(80A)": 162000,
-  "P.R CD-R 700MB": 25000,
-  "Rolo de Impressora termica": 1000,
-  "Adaptador Apple": 8000,
-  "Coluna Portable Stereo BT Speakers": 19500,
-  "TV BOX MXQ PRO 5G": 130000,
-  "Coluna Manhattan": 9000,
-  "Adaptador Alfa Network": 9000,
-  "Hik Vision (switch 8p)": 70000,
-  "Fonte de alimentaçâo de 8 canais": 19500,
-  "HDCP": 20000,
-  "Resma de papel (Ultima copy)": 22500,
-  "Fibra óptica": 75000,
-  "Microfone Lapela": 18500,
-  "HDTV Splitter VER 1.4": 38000,
-  "Haisenser Docking Station": 80000,
-  "Switch de 5 Porta 10/100Mbps": 12000,
-  "TP-LINK Omada AC1350 Wireless MU-MIMO GB": 102000,
-  "Teclado Númerico Mini": 11000,
-  "Conversor de Fibra Óptica D-LINK SC Monomodo": 12000,
-  "Cat6 RJ45": 25000,
-  "Kaspersky Standard/Anti vírus 5 dispositivos 1ano": 30000,
-  "DVD+R DL 8x": 19000,
-  "CD-R": 25000,
-  "Cabo Manhattan de UPS": 5000,
-  "Cabo HDMI 3 Metros": 5000,
-  "Cabo VGA": 5000,
-  "Ascultadores com microfone Genius": 16000,
-  "Switch de16+2+1 P0E": 180000,
-  "Adaptador Spliter USBC": 25000,
-  "BUCHAS": 1500,
-  "ADAPTADOR USB MICRO para USB-C": 3000,
-  "Carregador de Adroid normal": 1500,
+
   "Tinteiro GT53xL": 12000,
   "Tinteiro GT52 CYAN": 12000,
   "Tinteiro GT52 MAGENTA": 12000,
   "Tinteiro GT52 YELLO": 12000,
   "Tinteiro Tri-color 305": 15000,
-  "MUCHILA": 18000,
-  "ADAPTADOR DE HDMI USB": 20000,
-  "ADAPTADOR VGA HDTV": 12000,
-  "UNIÃO RJ45 PARA CABO DE REDE": 3500,
-  "HDMI SPLITTER 1*4 1xEntrada 4xSaidas 4k 30Hz": 59000,
-  "MICROSOFT CORPORATION": 473700,
-  "VIDEO BALUN Hikvision DS 1H18S/": 6000,
-  "CABO SAS para SATA/MOLEX70CM": 11500,
-  "COLA TERMICA": 2000,
+
+  "TP-LINK 300mbps wirellss N router": 24910,
+  "Router wifi Tp-link 300Mbsp 2x Antenas 1x portas 10/100 WAN, 4x portas 10/100LAN": 24910,
+
+  "router tp link lb link wr 450 h": 40000,
+
+  "TP-LINK AC1200": 200000,
+
+  "TP-LINK PoE Switch": 87210,
+
+
+
+  "Controladora wi-fi Tp-link OC 200 Omada Gestão centralizada de rede wifi accesso cloud": 113000,
+
+  "TP-Link Omada Hardware Controller OC200": 113000,
+
+  "adaptador wifi alfa": 9000,
+  "Adaptador Alfa Network": 9000,
+
+  "roteador industrial teltonika": 120000,
+
+  "repetidor de sinal 2x antena exterior sem fio wireless - N wifi": 20000,
+
+
+ "Bastidor de Rede Intellinet 09U (ALP-485x600x450) 19 com Porta de Vidro":180000,
+ "Cabo de Rede CAT6":85000,
+
+  "Rack de Servidor": 180000,
+
+
+  "Fibra óptica": 75000,
+
+  "Conversor de Fibra Óptica D-LINK SC Monomodo": 12000,
+
+
+
+  "relogioponto": 280000,
+
+
+  "UPS Pulstar Line Interactive UPS": 19000,
+
+  "UPS KS Offline Torre 800VA Preta Wintech": 44000,
+
+  "Extensão UPS(EWENT)": 15000,
+
+  "Extensão UPS Ewent": 19000,
+
   "Bateria Kaise Standard VRLA": 17000,
-  "Proposta de aluguer de impressora": 790000,
-}
+
+  "bateria para ups kaise vlra 12 v 7ah f1": 17500,
+
+  "transformador de fonte de alimentação CCTV": 25000,
+
+  "Kaspersky Standard/Anti vírus 5 dispositivos 1ano": 30000,
+
+  "antivirus kaspery standard 5 dispositivos 1 ano": 30000,
+
+  
+
+  "HDTV Splitter VER 1.4": 38000,
+  "HDTV Splitter VER 1x4": 38000,
+
+  "HDMI SPLITTER 1*4 1xEntrada 4xSaidas 4k 30Hz": 59000,
+
+  "HDCP": 20000,
+
+ 
+  "Carregador Android": 1500,
+  "Carregador de Adroid normal": 1500,
+
+  "Base de carregador tipo c": 4500,
+
+  "Adaptador USB Micro": 3000,
+  "ADAPTADOR USB MICRO para USB-C": 3000,
+  "adaptador usb-c para usb-b micro": 3000,
+
+  "Adaptador Apple": 8000,
+
+ 
+
+  "Extensão USB 2.O 30M": 19000,
+
+  "Haisenser Docking Station": 80000,
+
+  "Teclado+ Rato GENIUS": 10000,
+
+  "TECLADO NGS": 13000,
+
+  "teclado numerico exbom": 10000,
+
+  "Teclado Númerico Mini": 11000,
+
+  "Mine Teclado controlo de acesso TV": 13000,
+
+  "Base de Mause (EWENT)": 5000,
+
+  "Telefone Analogico (IPPRO )": 9000,
+
+  "Auriculares com fio (AEG)": 15000,
+
+  "Auricular sem fio Redmi": 15000,
+
+  "Ascultadores com microfone Genius": 16000,
+
+  "Microfone Lapela": 18500,
+
+  "Coluna Manhattan": 6000,
+
+  "Coluna Portable Stereo BT Speakers": 19500,
+
+  "Gaveta de dinheiro": 57000,
+
+  "Rolo de Impressora termica": 1000,
+
+  "Rolos de TPA": 500,
+
+
+  "Resma de papel (Ultima copy)": 22500,
+
+  "cassete de etiquer dymo D1 de 9mm": 10000,
+  "Alicate Grimpar": 68000,
+
+  "Alicate de crimpar RJ45": 18000,
+
+  "Computador Portátil": 495000,
+
+  "PILHA MAXELL": 600,
+
+  "pilha Maxell Alcaline": 1500,
+  "Impressora térmica X-Printer":80000,
+  "Alicate de Crimpar RJ45": 18000,
+"Colunas Manhattan": 6000,
+"HDTV Splitter Ver 1x4":38000,
+
+"Pilha AA LR06 Maxell Caixa com 4 Unidades": 1200,
+"Conector RJ45":18000,
+  "pilha relogio": 600,
+  "Pilha Maxell 9V": 1400,
+"Cabo de Rede CAT6 Soho Plus": 85000,
+"Pen drive (Maxell) 32 GB venture USB 2.0":0,
+
+  "Hikvision DVR DS-7216HGHI-M1/T 16 Canais Turbo HD 1080p Lite": 87552,
+
+  "VIDEO BALUN Hikvision DS 1H18S/": 6000,
+
+  "Fonte de alimentaçâo de 8 canais": 19500,
+
+
+  "Switch de 5 Porta 10/100Mbps": 12000,
+
+  "Switch de16+2+1 P0E": 180000,
+
+  "Hik Vision (switch 8p)": 70000,
+
+  "TV BOX MXQ PRO 5G": 130000,
+
+ 
+
+  "BUCHAS": 1500,
+
+  "COLA TERMICA": 2000,
+
+  "Adaptador Spliter USBC": 25000,
+
+  "Adaptador de Exibiçâo MINI": 8000,
+
+
+  "Memoria de Rede": 20000,
+
+  "Capa de memoria": 10000,
+
+  "MUCHILA": 18000,
+
+
+  };
